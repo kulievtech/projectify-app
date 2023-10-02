4efc505dc595de022beb46a6d8c66964fe5c13d4
Tohir Kuliev <kuliev.tech@gmail.com>
2023-10-02 13:02:24 -0400

    set up the project

---
+++ src/routes/user.routes.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { userController } from "../controllers/user.controllers.js";
+const userRouter = Router();
+
+userRouter.post("/sign-up", userController.signUp);
+
+export { userRouter };
