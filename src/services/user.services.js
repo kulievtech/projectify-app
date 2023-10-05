c89124254a706240d2d0027e5235f0f4b0db7570
Tohir Kuliev <kuliev.tech@gmail.com>
2023-10-05 16:39:13 -0400

    create hashing algorithm for password

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -1,13 +1,38 @@
 import { prisma } from "../prisma/index.js";
+import { hashFunction, generateSalt } from "../utils/hash.js";
 
 class UserService {
     signUp = async (input) => {
         try {
+            const salt = generateSalt();
+            const hashedPassword = hashFunction(input.password + salt);
             await prisma.user.create({
-                data: input
+                data: { ...input, password: `${salt}.${hashedPassword}` }
             });
         } catch (error) {
             throw new Error(error);
+        }
+    };
+    login = async (input) => {
+        try {
+            const user = await prisma.user.findFirst({
+                where: {
+                    email: input.email
+                }
+            });
+
+            if (!user) {
+                throw new Error("Invalid Credentials");
+            }
+
+            const [salt, userHashedPassword] = user.password.split(".");
+            const hashedPassword = hashFunction(input.password + salt);
+
+            if (userHashedPassword !== hashedPassword) {
+                throw new Error("Invalid Credentials");
+            }
+        } catch (error) {
+            throw error;
         }
     };
 }
