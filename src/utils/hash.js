d95332b23dcc26e78ae5bc896d8c0c7def36cabb
Tohir Kuliev <kuliev.tech@gmail.com>
2023-10-08 18:22:28 -0400

    work on account-activation

--- src/utils/hash.js
+++ src/utils/hash.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcryptjs";
+import cryptojs from "crypto-js";
 
 class Bcrypt {
     hash = async (password) => {
@@ -12,4 +13,36 @@ class Bcrypt {
     };
 }
 
+class Crypto {
+    generateRandomString = () => {
+        const chars = "abcdefghijklmnopqrstuvwxyz";
+        let randomString = "";
+
+        for (let i = 0; i < 10; i++) {
+            const randomIdx = Math.floor(Math.random() * 26);
+
+            randomString += chars[randomIdx];
+        }
+
+        return new Date().toISOString() + randomString;
+    };
+
+    hash = (input) => {
+        const hash = cryptojs.SHA256(input);
+        const hashString = hash.toString(cryptojs.enc.Hex);
+        return hashString;
+    };
+
+    createToken = () => {
+        const randomString = this.generateRandomString();
+        const hashedString = this.hash(randomString);
+        return hashedString;
+    };
+
+    compare = (token, hashedToken) => {
+        return hashedToken === this.hash(token);
+    };
+}
+
 export const hasher = new Bcrypt();
+export const crypto = new Crypto();
