f36d26b76f468dedaa67690268a651cc02df1477
Tohir Kuliev <kuliev.tech@gmail.com>
2024-03-21 13:28:24 -0400

    Unify contributor status change

--- src/services/project.service.js
+++ src/services/project.service.js
@@ -170,7 +170,8 @@ class ProjectService {
             data: { projectId, teamMemberId },
             select: {
                 status: true,
-                joinedAt: true
+                joinedAt: true,
+                teamMemberId: true
             }
         });
         return data;
